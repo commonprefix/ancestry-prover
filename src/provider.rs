ea2ad82a980cab7d639cc64ebcca04b7b1e8ceef
Dimitris Lamprinos <pkakelas@gmail.com>
2024-04-04 15:10:04 +0300

    Remove get_block_proof from the ProofProvider

--- src/provider.rs
+++ src/provider.rs
@@ -21,12 +21,6 @@ pub trait ProofProvider: Sync + Send + 'static {
         state_id: &str,
         gindex: u64,
     ) -> Result<Proof, ProofProviderError>;
-    /// Fetches a proof from a specific g_index or a path to the beacon root of a specific block.
-    async fn get_block_proof(
-        &self,
-        block_id: &str,
-        gindex: u64,
-    ) -> Result<Proof, ProofProviderError>;
 }
 
 #[derive(Clone)]
@@ -70,19 +64,6 @@ impl ProofProvider for LoadstarProver {
         let req = format!(
             "{}/state_proof?state_id={}&gindex={}&network={}",
             self.rpc, state_id, gindex, self.network
-        );
-
-        self.get(&req).await
-    }
-
-    async fn get_block_proof(
-        &self,
-        block_id: &str,
-        gindex: u64,
-    ) -> Result<Proof, ProofProviderError> {
-        let req = format!(
-            "{}/block_proof/?block_id={}&gindex={}&network={}",
-            self.rpc, block_id, gindex, self.network
         );
 
         self.get(&req).await
@@ -132,40 +113,6 @@ mod tests {
         );
 
         let result = prover.get_state_proof("state_id", 1).await;
-        assert!(result.is_err());
-    }
-
-    #[tokio::test]
-    async fn test_get_block_proof() {
-        let (server, prover) = setup_server_and_prover();
-        let json_response = serde_json::to_string(&Proof::default()).unwrap();
-
-        server.expect(
-            Expectation::matching(all_of![
-                request::query(url_decoded(contains(("block_id", "block_id")))),
-                request::query(url_decoded(contains(("gindex", "1")))),
-            ])
-            .respond_with(status_code(200).body(json_response)),
-        );
-
-        let result = prover.get_block_proof("block_id", 1).await.unwrap();
-        assert_eq!(result, Proof::default());
-    }
-
-    #[tokio::test]
-    async fn test_get_block_proof_error() {
-        let (server, prover) = setup_server_and_prover();
-
-        server.expect(
-            Expectation::matching(all_of![
-                request::query(url_decoded(contains(("block_id", "block_id")))),
-                request::query(url_decoded(contains(("gindex", "1")))),
-            ])
-            .respond_with(status_code(400)),
-        );
-
-        let result = prover.get_block_proof("block_id", 1).await;
-
         assert!(result.is_err());
     }
 }
